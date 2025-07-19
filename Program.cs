64c18b31491c1428e91c0c6d8e67488a5a897b1e
snnik11 <snnikita11@gmail.com>
2025-07-19 12:19:45 +1000

    added recursive digit sum

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Recursive_Digit_Sum;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var rg = new RecDigitSum();
+           rg.superDigit("148", 3);
+        }
+    }
+}
---
+++ Recursive_Digit_Sum/RecDigitSum.cs
@@ -0,0 +1,44 @@
+﻿namespace Recursive_Digit_Sum
+{
+    public class RecDigitSum
+    {
+
+        /*
+         * Complete the 'superDigit' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts following parameters:
+         *  1. STRING n
+         *  2. INTEGER k
+         */
+
+        public  int superDigit(string n, int k)
+        {
+            long initialSum = 0;
+            foreach (char c in n)
+            {
+                initialSum += c - '0';
+            }
+
+            long total = initialSum * k;
+            Console.WriteLine(FindSum(total));
+            return FindSum(total);
+        }
+        public int FindSum(long num)
+        {
+            if (num < 10)
+            {
+                return (int)num;
+            }
+            long sum = 0;
+
+            while (num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+            return FindSum(sum);
+        }
+
+    }
+}
